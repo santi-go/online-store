1568c8d20bbff3e41fa195ab76150233c1f5458d
Santiago <santiago.gonzalez.erazo@gmail.com>
2018-08-08 18:30:29 +0200

    Vuex done , bux to fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import VeeValidate from 'vee-validate'
 
 Vue.use(VeeValidate)
@@ -10,6 +11,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  strict: true,
+  state: {
+    // bought items
+    cart: [],
+    // ajax loader
+    showLoader: false,
+    // selected product
+    product: {},
+    // all products
+    products: [],
+    // all manufacturers
+    manufacturers: []
+  }
+})
